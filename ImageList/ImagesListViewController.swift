fa4c1a71fbb87bc7b37e93a8b3986c8d0713fe78
fEgor <fegor1818@icloud.com>
2024-10-01 12:01:23 +0200

    добавил констрейнты для всех элементов

--- ImageList/ImagesListViewController.swift
+++ ImageList/ImagesListViewController.swift
@@ -2,12 +2,15 @@ import UIKit
 
 final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 200
-        
-    }
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+       }
+
     func configCell(for cell: ImagesListCell) { }
 }
 extension ImagesListViewController: UITableViewDataSource {
