4753d0377675df59f6171c93f6c2065587decb7e
fEgor <fegor1818@icloud.com>
2024-10-07 14:15:54 +0200

    добавлены: view controller и ui элементы. выставлены констрейнты. добавлен новый класс ProfileViewController

---
+++ ImageList/ProfileViewController.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+    @IBOutlet private var avatarImage: UIImageView!
+}
